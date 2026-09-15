a804bde7f261c3c06629b6849ce62ebb210c940d
harshaldhote155 <harshaldhote155@gmail.com>
2026-09-15 16:04:09 +0530

    Delete 03_Assignment directory

--- 03_Assignment/Q1.py
+++
@@ -1,9 +0,0 @@
-# 1. Write a program to check if the given number is positive or negative.
-
-Num_input=int(input("Enter the Number:-"))
-
-if(Num_input>0):
-    print("The Number is Positive.")
-
-else:
-    print("The Number is Negative.")
